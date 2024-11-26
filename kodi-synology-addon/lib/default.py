39834fca214618ee9249c9d658e1d7390dbbc960
domovfilmy <krystofrubes2013@gmail.com>
2024-11-26 16:12:28 +0100

    Delete kodi-synology-addon directory

--- kodi-synology-addon/lib/default.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def connect_to_synology():
-    url = "https://quickconnect.to/krystofserver"
-    response = requests.get(url)
-    if response.status_code == 200:
-        print("Připojeno k Synology NAS")
-    else:
-        print("Chyba při připojení")
